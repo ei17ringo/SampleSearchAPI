d5405dc94993ec336c8652049f040aed2538d3bd
ei17ringo <ei17ringo@gmail.com>
2015-02-02 14:17:18 +0800

    Initial Commit

---
+++ SampleSearchAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleSearchAPI
+//
+//  Created by Eriko Ichinohe on 2015/02/02.
+//  Copyright (c) 2015年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
